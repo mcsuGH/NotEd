e85535138fdbe5a95045d88798d7e1ca5ded9664
Michael Su <mailmsuu@gmail.com>
2022-06-14 21:21:52 +0100

    Fix for Heroku

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, "client/build")));
+app.use(express.static(path.join(__dirname, "client/build")));
 
 if (process.env.NODE_ENV === "development") {
   app.use(
