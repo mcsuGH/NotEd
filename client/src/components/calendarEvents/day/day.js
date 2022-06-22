fc1768b040f5df96bfe66c06226babbedbc9a4d7
Michael Su <mailmsuu@gmail.com>
2022-06-22 20:47:25 +0100

    Remove useEffect from Day component
    Removed the unneccessary useEffect to update the events listings - it does it even without it

--- client/src/components/calendarEvents/day/day.js
+++ client/src/components/calendarEvents/day/day.js
@@ -1,31 +1,18 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext } from "react";
 import dayjs from "dayjs";
 import CalendarGlobalContext from "../../../context/calendarGlobalContext";
 
 export default function Day({ day, _key, rowIdx, events }) {
 
-  const listEvents = () => {
-    return (
-      <div>
-        {events.map((event, key) => {
-          return (
-            <div key={key}>
-              {event.date === day.format("DD-MM-YY") && event.title}
-            </div>
-          )
-        })}
-      </div>
-    )
-  }
-
-  useEffect(() => {
-    console.log(events)
-    listEvents()
-  }, [events])
-
   return (
     <div>
-      {listEvents()}
+      {events.map((event, key) => {
+        return (
+          <div key={key}>
+            {event.date === day.format("DD-MM-YY") && event.title}
+          </div>
+        )
+      })}
     </div>
   )
 }
