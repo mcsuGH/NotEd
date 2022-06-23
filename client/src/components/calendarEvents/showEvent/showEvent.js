f6bf6b1583c9c4975f8735cb91f6e24fbe1efa72
Michael Su <mailmsuu@gmail.com>
2022-06-23 21:45:59 +0100

    Close showEvent modal after Delete

--- client/src/components/calendarEvents/showEvent/showEvent.js
+++ client/src/components/calendarEvents/showEvent/showEvent.js
@@ -17,14 +17,15 @@ export default function ShowEvent( {url, event, setEventSelected, setEvents} ) {
       .then(setEvents((prevEvents) => {
         return prevEvents.filter((event) => event._id !== eventId)
       }))
+      .then(setEventSelected(null))
   }
 
   return (
     <div className="h-screen w-full fixed left-0 top-0 flex justify-center items-center">
       <div className="bg-white rounded-lg shadow-2xl w-1/3">
-        <header className={`bg-${event.label}-200 px-4 py-2 flex justify-between items-center`}>
-          <span className="text-gray-500">{labelValues[event.label]}</span>
-          <button className="text-gray-500" onClick={()=>setEventSelected(null)}>X</button>
+        <header className={`bg-${event.label}-500 px-4 py-2 flex justify-between items-center`}>
+          <span className="text-white">{labelValues[event.label]}</span>
+          <button className="text-white" onClick={()=>setEventSelected(null)}>X</button>
         </header>
         <div className="p-3">
           <div className="grid items-end gap-y-7">
