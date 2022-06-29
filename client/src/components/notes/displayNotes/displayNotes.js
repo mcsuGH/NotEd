b25adc548bbfc26139c0f04db1fd5314a244e324
Michael Su <mailmsuu@gmail.com>
2022-06-29 12:47:58 +0100

    Notes now display in rows of 5 with background colour for first 10 notes

--- client/src/components/notes/displayNotes/displayNotes.js
+++ client/src/components/notes/displayNotes/displayNotes.js
@@ -5,17 +5,32 @@ export default function DisplayNotes({ notes }) {
     return dayjs(date).format("DD/MM/YY")
   }
 
+  const noteColour = {
+    0: "indigo-200",
+    1: "red-200",
+    2: "blue-200",
+    3: "green-200",
+    4: "purple-200",
+    5: "purple-500",
+    6: "indigo-500",
+    7: "red-500",
+    8: "blue-500",
+    9: "green-500",
+  }
+
   const displayNote = (noteInfo, key) => {
     return (
       <div className={`note-${key}`} key={key.toString()}>
-        <div className="noteTitle">
-          {noteInfo.title}
-        </div>
-        <div className="noteDescription">
-          {noteInfo.description}
-        </div>
-        <div className="noteDate">
-          {formatDate(noteInfo.createdAt)}
+        <div className={`bg-${noteColour[key]}`}>
+          <div className="noteTitle">
+            {noteInfo.title}
+          </div>
+          <div className="noteDescription">
+            {noteInfo.description}
+          </div>
+          <div className="noteDate">
+            {formatDate(noteInfo.createdAt)}
+          </div>
         </div>
       </div>
     )
@@ -30,11 +45,13 @@ export default function DisplayNotes({ notes }) {
       return (
         <div>
           Your Notes:
-          {notes.map((noteInfo, key) => {
-            return (
-              displayNote(noteInfo, key)
-            )
-          })}
+          <div className= "flex-1 grid grid-cols-5 grid-rows-2">
+            {notes.map((noteInfo, key) => {
+              return (
+                displayNote(noteInfo, key)
+              )
+            })}
+          </div>
         </div>
       )
     }
@@ -42,9 +59,7 @@ export default function DisplayNotes({ notes }) {
 
   return (
     <div>
-      <div className="notes">
-        {checkNotes()}
-      </div>
+      {checkNotes()}
     </div>
   )
 }
